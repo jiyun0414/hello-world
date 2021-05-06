e59f3ac337d450e1bc7289a91cd309c01deef8c0
jiyun0414 <83629615+jiyun0414@users.noreply.github.com>
2021-05-06 16:40:48 +0900

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,12 @@
+x = "Hello"                   # 변수 x에 문자 Hello 선언
+y = 'Bey'                     #변수 y에 문자 Bey 선언
+= """안녕하세요.               
+저는 21학번 새내기 입니다"""   # 변수 z에 문자 2행 선언
+
+print(x)
+print(y)
+print(z)
+print("\n")                   # 공백 행
+print(x+y+z)
+print("\n")                   #공백 행
+print(z)
