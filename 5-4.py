ae4a8642b3e4ce5bee1b6553b7df3b5c22984453
jiyun0414 <83629615+jiyun0414@users.noreply.github.com>
2021-05-08 03:22:03 +0900

    Create 5-4.py

---
+++ 5-4.py
@@ -0,0 +1,16 @@
+# 불리언 3rd- 조건문 if else, eilf, else
+a = 3
+b = 2
+c = 5
+d = c - a
+
+if a > b :
+    print("Hello o_o")
+if a > c:
+    print("What?")
+elif a == d:
+    print("Bye~!")
+else:
+    print("Hi!")
+# 여러줄 주석 Shift + Alt + A
+# 한줄한줄 Ctrl + /
